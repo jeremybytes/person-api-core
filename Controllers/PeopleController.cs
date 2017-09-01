500c011086152f00de00d33b827615b38b5a9e3e
Jeremy Clark <jeremy@Jeremys-MacBook-Pro.local>
2017-09-01 09:24:21 -0700

    Initial Project

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CorePersonApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePersonApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class PeopleController : Controller
+    {
+        List<Person> people = People.GetPeople();
+
+        // GET api/<controller>
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return people;
+        }
+    }
+}
